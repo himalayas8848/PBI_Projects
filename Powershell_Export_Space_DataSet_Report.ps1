e1b34c1ec26c15a4ff6af939bfbb5d51a263efd4
dave <web8341@gmail.com>
2020-05-18 22:16:27 -0400

    created files operating PBI services

---
+++ Powershell_Export_Space_DataSet_Report.ps1
@@ -0,0 +1,39 @@
+﻿cls
+
+ $tentantWorkspaces = Get-PowerBIWorkspace -Scope Organization -Filter ("tolower(type) eq 'group'")
+
+Write-Host "There are --" $tentantWorkspaces.Count "-- workspaces in this Power BI tenant."
+
+# List out all workspace not deleted
+$spaces = Get-PowerBIWorkspace -Scope Organization -Filter ("tolower(state) ne 'deleted'")
+
+foreach ($space in $spaces)
+
+{
+
+    write-host $space.Id "|" $space.Name "|" $space.Type "|" $space.State "|" $space.IsOnDedicatedCapacity "|" $space.CapacityId
+
+} 
+
+# List out all dataset
+
+$dss = Get-PowerBIDataset -Scope Organization
+
+foreach ($ds in $dss)
+
+{
+
+    Write-Host "|"$ds.Id "|" $ds.name "|" $ds.ConfiguredBy "|" $ds.IsOnPremGatewayRequired
+
+}
+
+# List out all reports
+ $reports = Get-PowerBIReport -Scope Organization
+
+foreach ($report in $reports)
+
+{
+
+    Write-Host "|"$report.Id "|" $report.name "|" $report.DatasetId
+
+} 
